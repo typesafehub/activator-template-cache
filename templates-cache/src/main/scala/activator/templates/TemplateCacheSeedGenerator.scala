c0dd05f3c69e0d8efd34d57d049f2239a1a559fb
Henrik Engstrom <henrik.engstrom@gmail.com>
2015-02-03 11:57:19 -0500

    Adds capability to specify templates used to override the standard featured ones

--- templates-cache/src/main/scala/activator/templates/TemplateCacheSeedGenerator.scala
+++ templates-cache/src/main/scala/activator/templates/TemplateCacheSeedGenerator.scala
@@ -10,36 +10,37 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.{ Future, Await }
 import java.net.URI
 
-/** This is a class that can be used within an sbt build to seed an Activator installation with pre-fetched templates and index.*/
+/** This is a class that can be used within an sbt build to seed an Activator installation with pre-fetched templates and index. */
 object TemplateCacheSeedGenerator {
 
   case class Arguments(
     localDirectory: File = new File("cache-repo"),
-    remoteUri: URI = new URI("http://downloads.typesafe.com/typesafe-activator"))
+    remoteUri: URI = new URI("http://downloads.typesafe.com/typesafe-activator"),
+    templates: Seq[String] = Seq.empty[String])
 
   def parseUsage(args: Array[String]): Arguments = {
     def parseImpl(args: Arguments, remaining: List[String]): Arguments =
       remaining match {
         case "-remote" :: remoteUri :: rest => parseImpl(args.copy(remoteUri = new URI(remoteUri)), rest)
-        case file :: Nil => args.copy(localDirectory = new File(file))
+        case "-file" :: file :: rest => parseImpl(args.copy(localDirectory = new File(file)), rest)
+        case "-templates" :: templateNames :: Nil =>
+          if (templateNames != "") args.copy(templates = templateNames.split(",").map(_.trim()))
+          else args
         case unknown =>
           sys.error(s"""Unknown argument: $unknown
-            Usage:  TemplateCacheSeedGenerator (-remote <uri>) <cache directory>
+            Usage:  TemplateCacheSeedGenerator -remote <uri> -file <cache directory> -templates <template names separated by ",">
           """)
       }
     parseImpl(Arguments(), args.toList)
   }
 
   def main(args: Array[String]): Unit = {
-    // TODO - Actually parse stuff
     val arg = parseUsage(args)
     // TODO - Read in config for this.
     val cacheDir = arg.localDirectory
     // TODO - Pull this from config?
     implicit val timeout = akka.util.Timeout(120, TimeUnit.SECONDS)
-
     val system = akka.actor.ActorSystem()
-
     val remoteRepo = new repository.UriRemoteTemplateRepository(arg.remoteUri, system.log)
 
     // For futures
@@ -52,9 +53,22 @@ object TemplateCacheSeedGenerator {
           location = cacheDir,
           remote = remoteRepo,
           autoUpdate = false)
+
+      val templateList =
+        if (arg.templates.isEmpty) cache.featured
+        else {
+          cache.metadata.map { it =>
+            val filtered = it.filter { tpl =>
+              arg.templates.contains(tpl.name)
+            }
+            if (filtered.toList.length != arg.templates.length) throw new RuntimeException(s"Could not find template(s) matching name(s): ${arg.templates.mkString(",")}")
+            filtered
+          }
+        }
+
       val templates =
         for {
-          templates <- cache.featured
+          templates <- templateList
           results <- Future.traverse(templates map (_.id))(cache.template)
         } yield results.toSeq.flatten
       Await.result(templates, duration)
